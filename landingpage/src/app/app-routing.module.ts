260acdf7ce91476ec2807560478656f25e3c150d
nagapremika <sadhupremika@gmail.com>
2019-08-27 10:40:37 +0530

    commiting landing page

---
+++ landingpage/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+
+
+
+
+const routes: Routes = [
+  {path:'',component:HeroComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about', component:AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ landingpage/src/app/modals/User.ts
@@ -0,0 +1,7 @@
+export interface User{
+    id : number;
+    name : String;
+    email : String;
+    password : String;
+    designation : String;
+ }
