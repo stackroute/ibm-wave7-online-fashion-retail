c98a71457462bf5c98ffdc548713cda8fcdf985f
velsuripoornima <velsuripoornima@github.com>
2019-08-29 10:08:27 +0530

    designer dashboard

--- fashionretail-frontend/src/app/app-routing.module.ts
+++ fashionretail-frontend/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { DesignerHomePageComponent } from './designer-home-page/designer-home-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -11,7 +13,11 @@ import { AboutComponent } from './about/about.component';
 const routes: Routes = [
   {path:'',component:HeroComponent},
   {path:'contact',component:ContactComponent},
-  {path:'about', component:AboutComponent}
+  {path:'about', component:AboutComponent},
+  {
+    path: 'designer',
+    component: DesignerHomePageComponent
+  }
 ];
 
 @NgModule({
