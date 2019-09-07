c9808068ca23025f8e0b6cd8cad6741650987624
Sindhura-Majji <sindhura.majji@gmail.com>
2019-09-07 17:16:08 +0530

    resolved conflicts

--- login1/src/main/java/com/stackroute/login/config/JwtTokenUtil.java
+++ login1/src/main/java/com/stackroute/login/config/JwtTokenUtil.java
@@ -3,7 +3,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
