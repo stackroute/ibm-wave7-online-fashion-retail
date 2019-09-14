e2fb56e5ea2228df2e21c3a33eac28c1e40fa866
thotakura anuradha <thotakura.anuradha@stackroute.in>
2019-09-14 21:02:02 +0530

    Integrated eureka and config

--- user-service/src/main/java/com/stackroute/onlinefashionretail/user/UserApplication.java
+++ user-service/src/main/java/com/stackroute/onlinefashionretail/user/UserApplication.java
@@ -1,8 +1,11 @@
 package com.stackroute.onlinefashionretail.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
 
