a474c5727233b182eb926f3cf471432edaffadfa
thotakura anuradha <thotakura.anuradha@stackroute.in>
2019-09-14 20:13:07 +0530

    Integrated eureka and config

--- consumer-service/src/main/java/com/stackroute/onlinefashionretail/consumer/ConsumerApplication.java
+++ consumer-service/src/main/java/com/stackroute/onlinefashionretail/consumer/ConsumerApplication.java
@@ -2,6 +2,9 @@ package com.stackroute.onlinefashionretail.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
 
 @SpringBootApplication
 public class ConsumerApplication {
