438d878e0e348a72ac7b9eb6d90f1a1d3147e00b
thotakura anuradha <thotakura.anuradha@stackroute.in>
2019-09-14 21:05:49 +0530

    Integrated eureka and config

--- workflow-service/src/main/java/com/stackroute/onlinefashionretail/workflow/WorkflowApplication.java
+++ workflow-service/src/main/java/com/stackroute/onlinefashionretail/workflow/WorkflowApplication.java
@@ -12,7 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class WorkflowApplication implements CommandLineRunner {
 
